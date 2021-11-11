568cd33c111c9a5aa51385edfc89be41e33c146a
jeremybutt09 <jbutt09@gmail.com>
2021-11-11 13:00:55 -0700

    modified the script to have an output of a table with PR by rep and an accompanying graph as well. Using DT and Ploty

--- scripts/shiny_dashboard.R
+++ scripts/shiny_dashboard.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(plotly)
 
 #PACKAGE TO CONSIDER DT, LEAFLET, AND PLOTLY
 
@@ -7,31 +8,68 @@ ui <- fluidPage(
   titlePanel("Workout Data Analysis"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "body_part",
-                  label = "Choose a Body Part",
-                  selected = "Chest",
-                  choices = c("Chest", "Arms", "Legs", "Back", "Shoulders"))
+      selectInput(inputId = "exercise",
+                  label = "Choose an Exercise",
+                  selected = "BACK SQUAT",
+                  choices = c("BACK SQUAT (BARBELL)", "DEADLIFT (BARBELL)", "FLAT BENCH PRESS (BARBELL)", "LAT PULLDOWN (MACHINE)", "STANDING SHOULDER PRESS (BARBELL)"),
+                  multiple = FALSE)
     ),
     mainPanel(
-      DT::DTOutput("workout_data")
+      DT::DTOutput("pr_data"),
+      plotly::plotlyOutput("plot_pr_trends")
     )
   )
 )
 
 server <- function(input, output, session) {
-  wo_data <- function() {
-    setwd("/Users/jeremybutt/workout_data")
+  pr_data <- function() {
+    setwd("C:/Users/Jeremy/Documents/workout_data")
     wo_data <- read_csv(file = "data/workout_output.csv")
     ref_data <- read_csv(file = "reference/muscle_ref.csv")
     
     wo_data %>%
-      left_join(ref_data, by = c("exercise_name" = "exercise_name")) %>%
-      filter(str_detect(string = generic_muscle_group,
-                        pattern = str_to_upper(input$body_part))) %>%
-      top_n(10, weight)
+      filter(reps != 0,
+             exercise_name == input$exercise) %>%
+      group_by(exercise_name,
+               reps) %>%
+      summarize(pr = max(weight)) %>%
+      inner_join(wo_data, by = c("exercise_name", "reps")) %>%
+      group_by(exercise_name,
+               reps,
+               pr) %>%
+      summarize(date = max(workout_date)) %>%
+      transmute(exercise_name,
+                reps,
+                pr_details = paste("PR:", pr, "LBS", "DATE:", date)) %>%
+      pivot_wider(names_from = exercise_name,
+                  values_from = pr_details) %>%
+      arrange(reps)
   }
-  output$workout_data <- DT::renderDT({
-    DT::datatable(wo_data())
+  
+  pr_trends <- function() {
+    setwd("C:/Users/Jeremy/Documents/workout_data")
+    wo_data <- read_csv(file = "data/workout_output.csv")
+    
+    wo_data %>%
+      filter(reps != 0) %>%
+      group_by(exercise_name,
+               reps,
+               workout_date) %>%
+      summarize(pr = max(weight)) %>%
+      filter(exercise_name %in% input$exercise) %>%
+      #mutate(reps = as.character(reps)) %>%
+      ggplot(aes(x = workout_date, y = pr, color = as.character(reps))) +
+      geom_line() +
+      geom_point()
+    
+  }
+  
+  output$pr_data <- DT::renderDT({
+    DT::datatable(pr_data())
+  })
+  
+  output$plot_pr_trends <- plotly::renderPlotly({
+    pr_trends()
   })
 }
 
