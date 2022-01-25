6c7d95c63a04a236e1687e0bf8c7386fca852bfd
Jeremy Butt <jbutt09@gmail.com>
2022-01-24 20:56:46 -0700

    file for weight data ETL

---
+++ scripts/weight_etl.R
@@ -0,0 +1,55 @@
+#AUTHOR: JEREMY BUTT
+#DATE CREATED: 2021-09-15
+
+#UPDATE: SEE GIT COMMENTS
+
+#KNOWN ISSUES/LIMITATION:
+#-NO WAY OF CHECKING IF DATA BEING IMPORTED IS DUPLICATE.
+#-HAVE CODE CHECK IF DATA HAS ALREADY BEEN IMPORTED.
+
+#LIBRARY LOAD
+library(tidyverse)
+library(lubridate)
+
+#FILES FOR WEIGHT DATA. 
+
+#WEIGHT DATA HISTORIC
+weight_data_file_hist <- "data/Renpho-Jeremy Butt.csv"
+
+#NEWLY IMPORTED DATA.
+weight_data_file_current_file <- "/Users/jeremybutt/Downloads/Renpho-Jeremy Butt.csv"
+
+test <- list("data/Renpho-Jeremy Butt.csv",
+             "/Users/jeremybutt/Downloads/Renpho-Jeremy Butt.csv")
+
+#READING HISTORIC DATA INTO R
+weight_data_hist <- read_csv(weight_data_file_hist) %>%
+  rename_with(str_to_lower) %>%
+  rename_with(~ str_replace_all(.x,
+                               pattern = "%",
+                               replacement = "percent")) %>%
+  rename_with(~ str_replace_all(.x,
+                                pattern = "[[:punct:]]",
+                                replacement = " ")) %>%
+  rename_with(str_trim) %>%
+  rename_with(~ str_replace_all(.x,
+                              pattern = "[[:space:]]",
+                              replacement = "_"))
+
+names(weight_data_hist)
+
+#READING NEW DATA INTO R
+weight_data_file_current <- read_csv(weight_data_file_current_file) %>%
+  anti_join(weight_data_hist, by = "Time of Measurement") #REMOVING DUPLICATE DATA
+
+weight_data <- weight_data_hist %>%
+  bind_rows(weight_data_file_current)
+
+weight_data <- weight_data %>%
+  mutate(date = mdy_hms(time_of_measurement))
+
+
+glimpse(weight_data)
+
+
+plot(x = weight_data$date, y = weight_data$weight_lb)
